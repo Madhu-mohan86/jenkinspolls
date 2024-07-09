e037167b714409ed3281a50d47061e917638f2d6
Madhu mohan <madhumohanmadhumohan3@gmail.com>
2024-07-09 18:43:07 +0530

    modified for poll

--- Helloworld.java
+++ Helloworld.java
@@ -2,6 +2,6 @@ import java.util.*;
 
 class Helloworld{
     public static void main(String args[]){
-        System.out.println("i am phil gandolf and you are a clown");
+        System.out.println("i am phil gandolf and you are a clown for instance");
     }
 }
