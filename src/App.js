8c2181d867ceb302ff749eeab8fb80eb690e4c18
Madhu mohan <madhumohanmadhumohan3@gmail.com>
2024-08-12 08:57:03 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <h1>this is simple message for testing</h1>
+        <h1>if this is changed then the keel is working</h1>
       </header>
     </div>
   );
