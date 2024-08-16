e1f1d65f7f4cb93682a471f198e92173751c7c44
Madhu mohan <madhumohanmadhumohan3@gmail.com>
2024-08-16 16:45:48 +0530

    theolderchecksumverification

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import SampleMount from "./SampleMount";
+
 function App() {
   return (
     <div>
        <h1> version 0.3 </h1>
        <h1> after changes in the trigger and policy and made auto update</h1>
+       <SampleMount/>
     </div>
   );
 }
